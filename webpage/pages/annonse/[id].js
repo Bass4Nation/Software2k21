bb9842791480ef9373e5d320685821bc81e17646
FuzzyCookie97 <69955315+FuzzyCookie97@users.noreply.github.com>
2021-11-26 16:42:29 +0100

    Created a base for individual items

--- webpage/pages/annonse/[id].js
+++ webpage/pages/annonse/[id].js
@@ -6,7 +6,15 @@ import { useRouter } from 'next/router';
 
     return (
         <>
-        <h1>DYNAMISK</h1>
+        <picture>
+        <img src="" alt="Very nice antique" style="width:500px;"/>
+        </picture>
+
+        <h2>Tittel</h2>
+        <p>Kjøp nå pris: XXX</p> <p>Bud pris: XXX</p>
+        <button>Kjøp nå</button> <button>Gi bud</button>
+        <p>Beskrivelse....</p>
+
         </>
     )
 
